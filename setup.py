d28d47471f41a0afa9d48607ecdfacd2222b30c2
James Li <jameswli2021@gmail.com>
2023-06-05 14:24:04 -0400

    Created setup.py with basic details

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='finaletools',
+      version='0.0',
+      author='James Li',
+      author_email='jameswli2021@gmail.com',
+      py_modules=['finaletools'],
+      package_dir={'':'src'})
