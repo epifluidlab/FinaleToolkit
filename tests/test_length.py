b955b67a4b68756e83d84542bb0dbe1929e8750d
James Li <jameswli2021@gmail.com>
2023-06-06 14:43:53 -0400

    fixed test_length.py

--- tests/test_length.py
+++ tests/test_length.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import finaletools
 import numpy as np
 
-lengths = np.array(finaletools.frag_length("/users/lij3mv/Desktop/Jan28.hg19.mdups.bam", quality_threshold=))
+lengths = np.array(finaletools.frag_length("/users/lij3mv/Desktop/Jan28.hg19.mdups.bam"))
 with open(f'{os.getcwd()}/tests/test_frag_lengths.bin', 'wb') as f_out:
     lengths.tofile(f_out)
 
