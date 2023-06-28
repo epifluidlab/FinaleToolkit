c0bf35ba083a1592d22d48b3a34687389381182d
James Li <jameswli2021@gmail.com>
2023-06-28 10:31:06 -0400

    wip histogram

--- src/finaletools/utils/frag_len_hist.py
+++ src/finaletools/utils/frag_len_hist.py
@@ -11,24 +11,29 @@ def frag_len_hist(
     counts, bins = np.histogram(frag_lengths, bins=n_bins)
     max_count = np.max(counts)
     max_height = term_height - 2
-    block_height = max_height * 9    # height for utf-8 block elements
-    block_counts = np.round(counts / max_count * block_height)
+    block_height = max_height * 8 - 1    # height for utf-8 block elements
+    block_counts = np.rint(counts / max_count * block_height)
 
-    hist_array = np.zeros((max_height, n_bins), dtype=np.byte)
-    for i, block_count in range(n_bins), block_counts:
-        solid_height = block_count // 9
-        hist_array[-solid_height:] = 9
-        hist_array[-solid_height-1] = block_count % 9
+    hist_array = np.zeros((max_height, n_bins), dtype=int)
+    for i in range(n_bins):
+        block_count = block_counts[i]
+        solid_height = int(block_count // 8)
+        if solid_height != 0:
+            hist_array[-solid_height:, i] = 8
+        hist_array[-solid_height-1, i] = block_count % 8
 
     bars = {
         0: ' ',
-        1: '/u2581',
-        2: '/u2582',
-        3: '/u2583',
-        4: '/u2584',
-        5: '/u2585',
-        6: '/u2586',
-        7: '/u2587',
-        8: '/u2588'
+        1: '\u2581',
+        2: '\u2582',
+        3: '\u2583',
+        4: '\u2584',
+        5: '\u2585',
+        6: '\u2586',
+        7: '\u2587',
+        8: '\u2588'
     }
-
+    
+    for row in hist_array:
+        print(''.join([bars[num] for num in row]))
+    
