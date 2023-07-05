695adcb08d3a466e102bf6cf0d07a4da99086c88
James Li <jameswli2021@gmail.com>
2023-07-05 14:02:14 -0400

    Clean up handling different input_file types for coverage

--- src/finaletools/frag/coverage.py
+++ src/finaletools/frag/coverage.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import sys
 import time
 
 import pysam
@@ -11,7 +12,6 @@ def single_coverage(
         contig,
         start=0,
         stop=None,
-        output_file=None,
         quality_threshold=30,
         verbose=False):
     """
@@ -29,7 +29,6 @@ def single_coverage(
     contig : string
     start : int
     stop : int
-    output_file : string, optional
     quality_threshold : int, optional
     verbose : bool, optional
 
@@ -41,22 +40,32 @@ def single_coverage(
     # TODO: determine if reference (like as found in pysam) is necessary
     # TODO: consider including region string (like in pysam)
 
-    if (verbose):
+    if verbose:
         start_time = time.time()
 
     # initializing variable for coverage tuple outside of with statement
     coverage = 0
 
-    if (type(input_file) == pysam.AlignmentFile):
-        sam_file = input_file
-        # Iterating on each read in file in specified contig/chromosome
+    input_type = type(input_file)
+
+    try:
+        # Handling different input types
+        if input_type == pysam.AlignmentFile:
+            sam_file = input_file
+        elif input_type == str:
+            sam_file = pysam.AlignmentFile(input_file, 'r')
+        else:
+            raise TypeError('input_file should be str or pysam.AlignmentFile')
+
+        # Iterating on each read in file in
+        # specified contig/chromosome
         for read1 in sam_file.fetch(
             contig=contig,
-            start=None if (tempstart:=start-500) < 0 else tempstart,
+            start=0 if (tempstart:=start-500) < 0 else tempstart,
             stop=stop+500 if stop is not None else None):
-            # Only select forward strand and filter out non-paired-end
-            # reads and low-quality reads
-            if (not_read1_or_low_quality(read1, quality_threshold)):
+            # Only select forward strand and filter out
+            # non-paired-end reads and low-quality reads
+            if not_read1_or_low_quality(read1, quality_threshold):
                 pass
             else:
                 # calculate mid-point of fragment
@@ -73,34 +82,12 @@ def single_coverage(
                     coverage += 1
                 else:
                     pass
-    else:
-        with pysam.AlignmentFile(input_file, 'r') as sam_file:
-            # Iterating on each read in file in
-            # specified contig/chromosome
-            for read1 in sam_file.fetch(
-                contig=contig,
-                start=0 if (tempstart:=start-500) < 0 else tempstart,
-                stop=stop+500 if stop is not None else None):
-                # Only select forward strand and filter out
-                # non-paired-end reads and low-quality reads
-                if not_read1_or_low_quality(read1, quality_threshold):
-                    pass
-                else:
-                    # calculate mid-point of fragment
-                    center = read1.reference_start + read1.template_length // 2
-                    if start is None and stop is None:
-                        coverage += 1
-                    elif start is None:
-                        if center < stop:
-                            coverage += 1
-                    elif stop is None:
-                        if center >= start:
-                            coverage += 1
-                    elif (center >= start) and (center < stop):
-                        coverage += 1
-                    else:
-                        pass
-    if (verbose):
+    finally:
+        if input_type == str:
+            sam_file.close()
+
+
+    if verbose:
         end_time = time.time()
         print(f'frag_coverage took {end_time - start_time} s to complete')
 
