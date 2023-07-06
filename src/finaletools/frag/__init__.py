e8eac414116c0eab7f126185c6d86099cdc8e32f
James Li <jameswli2021@gmail.com>
2023-07-06 11:39:54 -0400

    add cli_hist to utils import

--- src/finaletools/frag/__init__.py
+++ src/finaletools/frag/__init__.py
@@ -1,18 +1,5 @@
-"""
-Author: James Li
-Created: 6/2/23
-PI: Yaping Liu
-
-Description:
-Python script to calculate fragment features given a BAM file.
-
-"""
-# TODO: typing annotations for all functions
-
-from __future__ import annotations
-import argparse
-
-from finaletools.frag.frag_length import frag_length
+from finaletools.frag.frag_length import single_frag_length
+from finaletools.frag.frag_len_hist import frag_len_hist
 from finaletools.frag.coverage import coverage
 from finaletools.frag.wps import wps
 from finaletools.frag.agg_wps import aggregate_wps
