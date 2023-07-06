424a61ba9b419390aec079a2ebc2b29fe5d34d9a
James Li <jameswli2021@gmail.com>
2023-07-06 11:36:05 -0400

    cli hist accepts bins and counts

--- src/finaletools/utils/cli_hist.py
+++ src/finaletools/utils/cli_hist.py
@@ -3,28 +3,12 @@ from shutil import get_terminal_size
 
 import numpy as np
 
-def frag_len_hist(
-        frag_lengths: np.ndarray
+def cli_hist(
+        bins: np.ndarray,
+        counts: np.ndarray
 ):
     term_width, term_height = get_terminal_size((80, 24))
-    n_bins = term_width - 12
-
-    start = np.min(frag_lengths)
-    stop = np.max(frag_lengths)
-
-    bin_size = round((stop - start) / n_bins)
-
-    print(bin_size)
-
-    n_bins = (stop - start) // bin_size
-
-    bins = np.arange(start, stop, bin_size)
-    counts = []
-    # generate histogram
-    for bin in bins:
-        count = np.sum((frag_lengths >= bin) * (frag_lengths < (bin + bin_size)))
-        counts.append(count)
-    bins = np.append(bins, stop)
+    n_bins = bins.shape[0]
 
     max_count = np.max(counts)
     max_height = term_height - 2
