4af9997e828258a97eeb2440fb1572fce7dac3f2
James Li <jameswli2021@gmail.com>
2023-07-17 16:22:17 -0400

    implemented region_end_motifs

--- src/finaletools/frag/end_motifs.py
+++ src/finaletools/frag/end_motifs.py
@@ -1,15 +1,40 @@
 from __future__ import annotations
 from typing import Union
 
+import py2bit
+import numpy as np
+from numpy.typing import NDArray
+
 from finaletools.utils.utils import frag_array
 
-# XXX: implement
+def _gen_kmers(k: int, bases: str) -> list:
+        """Function to recursively create a list of k-mers."""
+        if k == 1:
+            return [base for base in bases]
+        else:
+            kmers = []
+            for k_minus_mer in _gen_kmers(k-1, bases):
+                for base in bases:
+                    kmers.append(k_minus_mer+base)
+            return kmers
+
+
+def _reverse_complement(kmer: str) -> str:
+    reversed = kmer[-1::-1]
+    pair_dict = {'A': 'T',
+                 'T': 'A',
+                 'G': 'C',
+                 'C': 'G'}
+    complemented = ''.join(pair_dict[base] for base in reversed)
+    return complemented
+
+
 def region_end_motifs(
     input_file: str,
     contig: str,
     start: int,
     stop: int,
-    refseq: str,
+    refseq_file: str,
     k: int = 4,
     output_file: Union(None, str) = None,
     quality_threshold: int = 30,
@@ -18,7 +43,7 @@ def region_end_motifs(
     """
     Function that reads fragments int the specified region from a BAM,
     SAM, or tabix indexed file and returns the 5' k-mer (default is
-    4-mer) end motif frequencies as a dictionary.
+    4-mer) end motif counts as a structured array.
 
     Parameters
     ----------
@@ -26,7 +51,7 @@ def region_end_motifs(
     contig : str
     start : int
     stop : int
-    refseq : str
+    refseq_file : str
     k : int, optional
     output_file : None or str, optional
     quality_threshold : int, optional
@@ -36,18 +61,54 @@ def region_end_motifs(
     ------
     end_motif_freq : dict
     """
-    return None
+    # numpy array of fragments
+    frag_ends = frag_array(
+        input_file,
+        contig,
+        quality_threshold,
+        start,
+        stop,
+        fraction_low=4,
+        fraction_high=100000,
+    )
+    # create dict where keys are kmers and values are counts
+    bases='ACGT'
+    kmer_list = _gen_kmers(k, bases)
+    end_motif_counts = dict(zip(kmer_list, 4**k*[0]))
+
+    # TODO: accept other reference file types e.g. FASTA
+    # count end motifs
+    try:
+        refseq = py2bit.open(refseq_file, 'r')
+        for frag in frag_ends:
+            # forward end-motif
+            forward_kmer = refseq.sequence(
+                contig, int(frag[0]), int(frag[0]+k)
+            )
+            if 'N' not in forward_kmer:
+                end_motif_counts[forward_kmer] += 1
+
+            # reverse end-motif
+            reverse_kmer = refseq.sequence(
+                contig, int(frag[1]-k), int(frag[1])
+            )
+            if 'N' not in reverse_kmer:
+                end_motif_counts[_reverse_complement(reverse_kmer)] += 1
+    finally:
+        refseq.close()
+
+    return end_motif_counts
 
 # XXX: implement
 def end_motifs(
     input_file: str,
-    refseq: str,
+    refseq_file: str,
     k: int = 4,
     output_file: Union(None, str) = None,
     quality_threshold: int = 30,
     workers: int = 1,
     verbose: Union(bool, int) = False,
-) -> dict:
+) -> NDArray:
     """
     Function that reads fragments from a BAM, SAM, or tabix indexed
     file and returns the 5' k-mer (default is 4-mer) end motif
@@ -56,7 +117,7 @@ def end_motifs(
     Parameters
     ----------
     input_file : str
-    refseq : str
+    refseq_file : str
     k : int, optional
     output_file : None or str, optional
     quality_threshold : int, optional
@@ -65,6 +126,6 @@ def end_motifs(
 
     Return
     ------
-    end_motif_freq : dict
+    end_motif_freq : list
     """
     return None
