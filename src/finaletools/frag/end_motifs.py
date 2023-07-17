101a037b52bd4c97e0a2e1d714458a674579d3a3
James Li <jameswli2021@gmail.com>
2023-07-17 14:20:34 -0400

    update api for end_motifs

--- src/finaletools/frag/end_motifs.py
+++ src/finaletools/frag/end_motifs.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 from typing import Union
 
+from finaletools.utils.utils import frag_array
 
+# XXX: implement
 def region_end_motifs(
     input_file: str,
     contig: str,
@@ -10,12 +12,29 @@ def region_end_motifs(
     refseq: str,
     k: int = 4,
     output_file: Union(None, str) = None,
+    quality_threshold: int = 30,
     verbose: Union(bool, int) = False,
 ) -> dict:
     """
     Function that reads fragments int the specified region from a BAM,
     SAM, or tabix indexed file and returns the 5' k-mer (default is
     4-mer) end motif frequencies as a dictionary.
+
+    Parameters
+    ----------
+    input_file : str
+    contig : str
+    start : int
+    stop : int
+    refseq : str
+    k : int, optional
+    output_file : None or str, optional
+    quality_threshold : int, optional
+    verbose : bool or int, optional
+
+    Return
+    ------
+    end_motif_freq : dict
     """
     return None
 
@@ -25,6 +44,7 @@ def end_motifs(
     refseq: str,
     k: int = 4,
     output_file: Union(None, str) = None,
+    quality_threshold: int = 30,
     workers: int = 1,
     verbose: Union(bool, int) = False,
 ) -> dict:
@@ -32,5 +52,19 @@ def end_motifs(
     Function that reads fragments from a BAM, SAM, or tabix indexed
     file and returns the 5' k-mer (default is 4-mer) end motif
     frequencies as a dictionary. Optionally writes data to a tsv.
+
+    Parameters
+    ----------
+    input_file : str
+    refseq : str
+    k : int, optional
+    output_file : None or str, optional
+    quality_threshold : int, optional
+    workers : int, optional
+    verbose : bool or int, optional
+
+    Return
+    ------
+    end_motif_freq : dict
     """
     return None
