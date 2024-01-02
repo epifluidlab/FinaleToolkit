7acfa9b3d9de1ee30f99fdc5075144aa19af5d6e
James Li <jameswli2021@gmail.com>
2024-01-01 20:19:58 -0500

    created empty cleavage profile module

---
+++ src/finaletools/frag/cleavage_profile.py
@@ -0,0 +1,12 @@
+"""Cleavage Profiler
+
+This module is used to find the cleavage profile as described in Zhou
+et al 2022 (https://doi.org/10.1073/pnas.2209852119). Cleavage profile
+describes the proportion of fragment ends at a site over the depth at
+the site (cleavage proportion) calculated over a 5+/- nt window around a
+CpG site.
+"""
+
+from __future__ import annotations
+
+# FIXME: implement
