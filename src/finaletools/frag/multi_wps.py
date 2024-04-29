c11881b9f3ebbc544b703253860fc7ff3cbb0eab
Ravi Bandaru <80599578+ravibandaru-lab@users.noreply.github.com>
2024-04-28 19:32:06 -0500

    Update multi_wps.py

--- src/finaletools/frag/multi_wps.py
+++ src/finaletools/frag/multi_wps.py
@@ -12,70 +12,41 @@ from tqdm import tqdm
 import pyBigWig as pbw
 
 from finaletools.frag.wps import wps
-
+from finaletools.utils.utils import _parse_chrom_sizes, _reduce_overlaps_in_file, _convert_to_list, _merge_all_intervals
 
 def _wps_star(args):
-    """Helper function to be used with imap"""
     return wps(*args)
 
 
 def multi_wps(
         input_file: Union[pysam.AlignmentFile, str],
-        site_bed: str,
-        output_file: Union[str, None]=None,
-        window_size: int=120,
-        interval_size: int=5000,
-        fraction_low: int=120,
-        fraction_high: int=180,
-        quality_threshold: int=30,
-        workers: int=1,
-        verbose: Union[bool, int]=0
+        interval_file: str,
+        output_file: Union[str, None],
+        window_size: int,
+        interval_size: int,
+        min_length: int,
+        max_length: int,
+        chrom_sizes: str,
+        quality_threshold: int,
+        intersect_policy: str,
+        workers: int,
+        verbose: bool
         ) -> np.ndarray:
-    """
-    Function that aggregates WPS over sites in BED file according to the
-    method described by Snyder et al (2016).
-
-    Parameters
-    ----------
-    input_file : str or pysam.AlignmentFile
-        BAM, SAM, or tabix file containing paired-end fragment reads or its
-        path. `AlignmentFile` must be opened in read mode.
-    site_bed: str
-        Bed file containing intervals to perform WPS on.
-    output_file : string, optional
-    window_size : int, optional
-        Size of window to calculate WPS. Default is k = 120, equivalent
-        to L-WPS.
-    interval_size : int, optional
-        Size of each interval specified in the bed file. Should be the
-        same for every interbal. Default is 5000.
-    fraction_low : int, optional
-        Specifies lowest fragment length included in calculation.
-        Default is 120, equivalent to long fraction.
-    fraction_high : int, optional
-        Specifies highest fragment length included in calculation.
-        Default is 120, equivalent to long fraction.
-    quality_threshold : int, optional
-    workers : int, optional
-    verbose : bool, optional
-
-    Returns
-    -------
-    scores : numpy.ndarray
-        np array of shape (n, 2) where column 1 is the coordinate and
-        column 2 is the score and n is the number of coordinates in
-        region [start,stop)
-    """
+   
     if (verbose):
         start_time = time.time()
         stderr.write(
             f"""
             Calculating aggregate WPS
             input_file: {input_file}
-            site_bed: {site_bed}
+            interval_file: {interval_file}
             output_file: {output_file}
             window_size: {window_size}
+            min_length: {min_length}
+            max_length: {max_length}
+            chrom_sizes: {chrom_sizes}
             interval_size: {interval_size}
+            intersect_policy: {intersect_policy}
             quality_threshold: {quality_threshold}
             workers: {workers}
             verbose: {verbose}
@@ -83,10 +54,9 @@ def multi_wps(
             """
         )
 
-    if (input_file == '-' and site_bed == '-'):
-        raise ValueError('input_file and site_bed cannot both read from stdin')
-
-    # get header from input_file
+    if (input_file == '-' and interval_file == '-'):
+        raise ValueError('The input file and interval file cannot both read from stdin')
+    
     if (input_file.endswith('.sam')
         or input_file.endswith('.bam')
         or input_file.endswith('.cram')):
@@ -94,63 +64,30 @@ def multi_wps(
             references = bam.references
             lengths = bam.lengths
             header = list(zip(references, lengths))
-    # TODO: get a header when reading tabix
     elif (input_file.endswith('.bed')
           or input_file.endswith('.bed.gz')
           or input_file.endswith('.frag')
           or input_file.endswith('.frag.gz')
     ):
-        with pysam.TabixFile(input_file, 'r') as tbx:
-            raise NotImplementedError('tabix files not yet supported!')
+        if chrom_sizes is None:
+            raise ValueError(
+                '--chrom_sizes must be specified for BED/Fragment files'
+            )
+        header = _parse_chrom_sizes(chrom_sizes)
     else:
         raise ValueError("Not a supported file type.")
 
     if (verbose > 1):
-        stderr.write(f'header is {header}\n')
+        stderr.write(f'Header is {header}\n')
 
-    # read tss contigs and coordinates from bed
     if (verbose):
         stderr.write('Reading intervals from bed\n')
 
-    contigs = []
-    starts = []
-    stops = []
-    try:
-        if site_bed == '-':
-            bed = stdin
-        else:
-            bed = open(site_bed)
-
-        # for overlap checking
-        prev_contig = None
-        prev_start = 0
-        prev_stop = 0
-        for line in bed:
-            contents = line.split()
-            contig = contents[0].strip()
-            start = int(contents[1])
-            stop = int(contents[2])
-
-            # cut off part of previous interval if overlap
-            if contig == prev_contig and start < prev_stop:
-                prev_stop = start
-
-            if prev_contig is not None:
-                contigs.append(prev_contig)
-                starts.append(prev_start)
-                stops.append(prev_stop)
-
-            prev_contig = contig
-            prev_start = start
-            prev_stop = stop
-        # appending last interval
-        contigs.append(prev_contig)
-        starts.append(prev_start)
-        stops.append(prev_stop)
-
-    finally:
-        if site_bed != '-':
-            bed.close()
+    reduced_intervals = _reduce_overlaps_in_file(interval_file)
+    converted_intervals = _convert_to_list(reduced_intervals)
+    all_intervals = _merge_all_intervals(converted_intervals)
+    contigs, starts, stops = zip(*all_intervals)
+    count = len(contigs)
 
     left_of_site = round(-interval_size / 2)
     right_of_site = round(interval_size / 2)
@@ -169,26 +106,21 @@ def multi_wps(
         stops,
         count*[None],
         count*[window_size],
-        count*[fraction_low],
-        count*[fraction_high],
+        count*[min_length],
+        count*[max_length],
+        count*[intersect_policy],
         count*[quality_threshold],
-        count*[verbose-2 if verbose>2 else 0]
+        count*[verbose]
     )
 
     if (verbose):
-        stderr.write('Calculating wps...\n')
+        stderr.write('Calculating WPS...\n')
 
     try:
-        pool = Pool(workers, maxtasksperchild=500)
-        # chunksize limited for memory
-        interval_scores = pool.imap(
-            _wps_star,
-            interval_list,
-            chunksize=100
-        )
+        pool = Pool(processes=workers)
+        interval_scores = pool.imap(_wps_star, interval_list, chunksize=max(len(contigs)//workers, 1))
 
-        # output
-        if (type(output_file) == str):   # check if output specified
+        if (type(output_file) == str):
             if (verbose):
                 stderr.write(
                     f'Output file {output_file} specified. Opening...\n'
@@ -197,7 +129,7 @@ def multi_wps(
             if output_file.endswith(".bw"): # BigWig
                 with pbw.open(output_file, 'w') as bigwig:
                     bigwig.addHeader(header)
-                    for interval_score in interval_scores:
+                    for interval_score in tqdm(interval_scores, desc="Adding intervals to bigWig file.", disable=not verbose):
                         contigs = interval_score['contig']
                         starts = interval_score['start']
                         scores = interval_score['wps']
@@ -223,30 +155,15 @@ def multi_wps(
                                 'encountered. Skipping to next.\n'
                             )
                             continue
-            elif (output_file.endswith('.bed.gz')
-                  or output_file.endswith('bedGraph.gz')):
-                with gzip.open(output_file, 'wt') as bedgraph:
-                    for interval_score in interval_scores:
-                        contigs = interval_score['contig']
-                        starts = interval_score['start']
-                        scores = interval_score['wps']
-                        stops = starts + 1
-
-                        lines = ''.join(f'{contig}\t{start}\t{stop}\t{score}\n'
-                                 for contig, start, stop, score
-                                 in zip(contigs, starts, stops, scores))
-
-                        bedgraph.write(lines)
-
-            else:   # unaccepted file type
+            else:
                 raise ValueError(
-                    'output_file can only have suffix .bw'
+                    'Output file can only have suffix .bw'
                     )
 
         elif (output_file is not None):
             raise TypeError(
-                f'output_file is unsupported type "{type(input_file)}". '
-                'output_file should be a string specifying the path of the '
+                f'The output file is unsupported type "{type(output_file)}". '
+                'Output file should be a string specifying the path of the '
                 'file to output scores to.'
                 )
     finally:
@@ -255,7 +172,7 @@ def multi_wps(
     if (verbose):
         end_time = time.time()
         stderr.write(
-            f'multi_wps took {end_time - start_time} s to complete\n'
+            f'Calculating WPS took {end_time - start_time} s to complete\n'
         )
 
     return scores
