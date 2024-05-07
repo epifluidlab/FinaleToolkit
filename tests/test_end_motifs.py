61b07c17f208e9b67029ff11af21ca0bd53f6362
James Li <jameswli2021@gmail.com>
2024-05-07 15:32:11 -0400

    update 2bit used

--- tests/test_end_motifs.py
+++ tests/test_end_motifs.py
@@ -8,6 +8,6 @@ from finaletoolkit.frag.end_motifs import *
 class TestGenomewideEndMotifs:
     def test_end_motifs(self, request):
         bam = request.path.parent / 'data' / '12.3444.b37.bam'
-        ref_file = request.path.parent / 'data' / '12_34443100-34446700.2bit'
+        ref_file = request.path.parent / 'data' / 'b37.chr12.2bit'
         motifs = end_motifs(
             bam, ref_file, both_strands=True, quality_threshold=0)
