8892826996a90e15c00dd16b2188a5bebf878b01
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2024-05-18 00:56:53 -0400

    remove unused and redundant function

--- src/finaletoolkit/utils/utils.py
+++ src/finaletoolkit/utils/utils.py
@@ -51,6 +51,7 @@ def _merge_overlapping_intervals(intervals):
             merged[-1] = (merged[-1][0], max(merged[-1][1], interval[1]))
     return merged
 
+
 def _reduce_overlaps_in_file(interval_file):
     intervals_dict = {}
     with open(interval_file, 'r') as file:
@@ -66,47 +67,19 @@ def _reduce_overlaps_in_file(interval_file):
         reduced_intervals[chrom] = _merge_overlapping_intervals(intervals)
     return reduced_intervals    
 
+
 def _convert_to_list(reduced_intervals):
     converted_intervals = {}
     for chrom, intervals in reduced_intervals.items():
         converted_intervals[chrom] = [[chrom, start, end] for start, end in intervals]
     return converted_intervals
 
+
 def _merge_all_intervals(converted_intervals):
     all_intervals = []
     for intervals in converted_intervals.values():
         all_intervals.extend(intervals)
     return all_intervals
-
-def get_contig_lengths(
-    input_file: Union[str, pysam.AlignmentFile],
-    verbose: bool
-) -> List[Tuple[str, int]]:
-
-    input_is_file = False
-    sam_file = None
-
-    try:
-        if isinstance(input_file, pysam.AlignmentFile):
-            sam_file = input_file
-        elif isinstance(input_file, str) and input_file.endswith(('bam', 'sam')):
-            input_is_file = True
-            if verbose:
-                stderr.write(f'Opening {input_file}\n')
-            sam_file = pysam.AlignmentFile(input_file)
-        else:
-            raise ValueError('Invalid input_file type. Only BAM or SAM files are allowed.')
-
-        contigs = sam_file.references
-        lengths = sam_file.lengths
-
-        contig_lengths = list(zip(contigs, lengths))
-
-    finally:
-        if input_is_file and sam_file is not None:
-            sam_file.close()
-
-    return contig_lengths
 
 
 def frag_bam_to_bed(input_file: Union[str, pysam.AlignmentFile],
