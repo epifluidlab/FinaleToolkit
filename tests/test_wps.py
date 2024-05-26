df635820aa0bd8d0facba0f1658d37ca0b42d259
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2024-05-26 16:38:02 -0400

    create lwps test

---
+++ tests/test_wps.py
@@ -0,0 +1,25 @@
+"""
+Tests for finaletoolkit.frag.wps, finaletoolkit.frag.adjust_wps, and
+finaletoolkit.frag.multi_wps
+"""
+
+import os
+import filecmp
+import difflib
+
+import pytest
+
+from finaletoolkit.frag.wps import *
+from finaletoolkit.frag.multi_wps import *
+from finaletoolkit.frag.adjust_wps import *
+
+class TestWPS:
+    def test_lwps(self, request):
+        bam = request.path.parent / 'data' / '12.3444.b37.bam'
+        results = wps(bam, '12', 34444145, 34444155, quality_threshold=0)
+
+        assert np.all(results['contig'] == '12')
+        assert np.all(
+            results['start'] == np.arange(34444145, 34444155)), str(results)
+        assert np.all(
+            results['wps'] == [-1,-1,-1,-1,-1,1,1,1,1,1]), str(results)
