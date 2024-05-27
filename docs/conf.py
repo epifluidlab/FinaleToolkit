51a01ab148050b513ea2b914e10ee470dc4e50a4
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2024-05-27 00:41:33 -0400

    add sphinx.ext.napoleon to parse numpy style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxarg.ext',
+    'sphinx.ext.napoleon'
 ]
 
 # -- Options for HTML output -------------------------------------------------
