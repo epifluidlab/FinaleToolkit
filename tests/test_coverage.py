7c49b56823ca52d7fca2d5cf5b9bd69d6d55da7a
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2024-05-30 12:43:26 -0400

    more coverage tests

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -20,6 +20,26 @@ class TestSingleCoverage:
         assert start == 0
         assert cov == pytest.approx(17)
 
+    def test_coverage_interval(self, request):
+        bam = request.path.parent / 'data' / '12.3444.b37.bam'
+        chrom, start, stop, name, cov = single_coverage(
+            bam, '12', 34443000, 34447000, quality_threshold=0)
+
+        assert chrom == '12'
+        assert start == 34443000
+        assert stop == 34447000
+        assert cov == pytest.approx(17)
+
+    def test_coverage_interval_midpoints(self, request):
+        bam = request.path.parent / 'data' / '12.3444.b37.bam'
+        chrom, start, stop, name, cov = single_coverage(
+            bam, '12', 34443400, 34443600, quality_threshold=0)
+
+        assert chrom == '12'
+        assert start == 34443400
+        assert stop == 34443600
+        assert cov == pytest.approx(2)
+
 class TestCoverage:
     def coverage(self, request):
         pass
