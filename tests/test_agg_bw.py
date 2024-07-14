d80573cccfe49baec84e60781d79545223e0301f
James Li <jameswli2021@gmail.com>
2024-07-14 09:55:35 -0400

    added tests for utils.agg_bw

---
+++ tests/test_agg_bw.py
@@ -0,0 +1,26 @@
+"""
+Tests for finaletoolkit.utils.agg_bw().
+"""
+
+import pytest
+
+from finaletoolkit.utils.agg_bw import agg_bw
+
+class TestAggBigWig:
+    def test_agg_bw(self, request, tmp_path):
+        """Test if it runs"""
+        bw = request.path.parent / 'data' / 'test.bw'
+        intervals = request.path.parent / 'data' / 'bw_test.bed'
+        dest = tmp_path / "out.wig"
+        scores = agg_bw(bw, intervals, dest, 0)
+
+        assert scores == pytest.approx([0.,0.,0.,0.,0.])
+
+    def test_median(self, request, tmp_path):
+        """Test median filter"""
+        bw = request.path.parent / 'data' / 'test.bw'
+        intervals = request.path.parent / 'data' / 'bw_test.bed'
+        dest = tmp_path / "out.wig"
+        scores = agg_bw(bw, intervals, dest, 2)
+
+        assert scores == pytest.approx([1.,2.,3.])
