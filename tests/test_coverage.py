498d5319cfbf81ae0501064ee307082767ea436b
James Li <jameswli2021@gmail.com>
2024-12-04 20:13:40 -0500

    formatting

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -14,7 +14,8 @@ from finaletoolkit.frag.coverage import *
 class TestSingleCoverage:
     def test_coverage(self, request):
         bam = request.path.parent / 'data' / '12.3444.b37.bam'
-        chrom, start, stop, name, cov = single_coverage(bam, '12', 0, None, quality_threshold=0)
+        chrom, start, stop, name, cov = single_coverage(bam, '12', 0, None,
+                                                        quality_threshold=0)
 
         assert chrom == '12'
         assert start == 0
@@ -44,7 +45,8 @@ class TestCoverage:
     def test_coverage_normalize(self, request):
         input_file = request.path.parent / 'data' / '12.3444.b37.frag.gz'
         intervals = request.path.parent / 'data' / 'intervals.bed'
-        results = coverage(input_file,intervals,"-",scale_factor=1.,normalize=True)
+        results = coverage(input_file ,intervals, "-", scale_factor=1.,
+                           normalize=True, verbose=True)
         for i in range(2):
             chrom, start, stop, name, cov = results[i]
             if i == 0:  
@@ -65,7 +67,7 @@ class TestCoverage:
         intervals = request.path.parent / 'data' / 'intervals.bed'
         results = coverage(input_file,intervals,"-", normalize=False,
                            intersect_policy='midpoint',
-                           scale_factor=1., verbose=True)
+                           scale_factor=1.)
         for i in range(2):
             chrom, start, stop, name, cov = results[i]
             if i == 0:  
