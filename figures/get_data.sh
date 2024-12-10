5972f95e93375bad18b4982d6c5f71ce335c73cb
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2024-12-10 12:54:53 -0600

    add some figure generation scripts

---
+++ figures/get_data.sh
@@ -0,0 +1,13 @@
+wget -P data/ https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.bam/content \
+https://zenodo.org/api/records/14284133/draft/files/tss.hg19.chr22.bed/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.frag.gz/content \
+https://zenodo.org/api/records/14284133/draft/files/tss.hg19.2kb.chr22.bw/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.cram.crai/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.frag.gz.tbi/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.sam/content \
+https://zenodo.org/api/records/14284133/draft/files/hg19.chrom.sizes/content \
+https://zenodo.org/api/records/14284133/draft/files/ENCFF001TDO.bed/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.cram/content \
+https://zenodo.org/api/records/14284133/draft/files/hg19.gap.delfi.final.txt/content \
+https://zenodo.org/api/records/14284133/draft/files/BH01.hg19.mdups.chr22.bam.bai/content \
+https://zenodo.org/api/records/14284133/draft/files/100kb.chr22.bins/content
