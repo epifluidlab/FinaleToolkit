164b6a327781c5fc0e3cef20f915b659f994f1c1
James Li <jameswli2021@gmail.com>
2024-12-18 15:43:31 -0500

    fix dependencies of agg_bw

--- src/finaletoolkit/utils/__init__.py
+++ src/finaletoolkit/utils/__init__.py
@@ -1,3 +1,3 @@
 from finaletoolkit.utils.utils import *
 from finaletoolkit.utils._filter_bam import filter_bam
-from finaletoolkit.utils.agg_bw import agg_bw
+from finaletoolkit.utils._agg_bw import agg_bw
