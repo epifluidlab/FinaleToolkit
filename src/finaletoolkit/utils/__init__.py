35ee656bf0866dfeba59bfc89c40c45fba7aad09
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2025-05-18 13:46:42 -0500

    sort_bed_file is imported to utils

--- src/finaletoolkit/utils/__init__.py
+++ src/finaletoolkit/utils/__init__.py
@@ -1,3 +1,4 @@
 from finaletoolkit.utils.utils import *
 from finaletoolkit.utils._filter_file import filter_file
 from finaletoolkit.utils._agg_bw import agg_bw
+from finaletoolkit.utils._sort_bed_file import sort_bed_file
